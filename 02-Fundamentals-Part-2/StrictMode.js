a8049021cfc583ba2389c2308be411c00c9c19c6
ameya-6964 <belvalkar7@gmail.com>
2023-12-23 00:15:19 +0530

    Added StrictMode

---
+++ 02-Fundamentals-Part-2/StrictMode.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriversLiscence = false;
+
+const passTest = true;
+
+if (passTest) hasDriversLiscence = true;
+
+if (hasDriversLiscence) console.log("I Can Drive");
+
+// const interface = "Audio"; Unexpected strict mode reserved word
