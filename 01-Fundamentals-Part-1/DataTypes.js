1d8d87e99351a825017cc3d328967e343e984954
ameya-6964 <belvalkar7@gmail.com>
2023-12-22 23:44:05 +0530

    Added Primitive DataTypes

---
+++ 01-Fundamentals-Part-1/DataTypes.js
@@ -0,0 +1,23 @@
+//! Values Are Only Primitive When They Are Not Objects
+/*
+ * Javascript Is Dynamically Typed Language\
+ * That Means Type Of Variable Is Not Neccessary To Be Used
+ */
+
+//! Number (Both floating and integers)
+let number = 1;
+
+//! Strings
+let firstName = `Ameya`;
+
+//! Booleans
+let isMarried = false;
+
+//! Undefined => Value Taken By Variable That Is Not Yet Defined
+let lastName;
+
+//! Null => Also Means Empty Value
+let salary = null;
+
+//! BigInt => Very Big Integers
+let bigNumber = new BigInt(1212121212121212);
