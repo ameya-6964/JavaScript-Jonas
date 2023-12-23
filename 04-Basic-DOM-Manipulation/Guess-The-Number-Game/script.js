ce66c800af8f5e6f00b3b0fc05f923ee79f8b901
ameya-6964 <belvalkar7@gmail.com>
2023-12-24 02:13:17 +0530

    Implemented No Number Logic

--- 04-Basic-DOM-Manipulation/Guess-The-Number-Game/script.js
+++ 04-Basic-DOM-Manipulation/Guess-The-Number-Game/script.js
@@ -1,7 +1,20 @@
 'use strict';
 
+/* 
 document.querySelector('.message').textContent = '🥳 Correct Number 🥳 ';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 // NOTE => Input Field Contains Value Property
-document.querySelector('.guess').value = 23;
+document.querySelector('.guess').value = 23; 
+*/
+
+const button = document.querySelector('.check');
+
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+});
