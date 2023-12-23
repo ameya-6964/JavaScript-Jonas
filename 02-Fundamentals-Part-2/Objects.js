433b96d3c4afb0e44f90d3d85cfeb32444cd58e0
ameya-6964 <belvalkar7@gmail.com>
2023-12-23 17:42:23 +0530

    Added Basics Of Objects

---
+++ 02-Fundamentals-Part-2/Objects.js
@@ -0,0 +1,35 @@
+//! Array Of Ameyas Information,But Information Is Lacking In This Array
+
+const ameyaArray = [
+  "Ameya",
+  "Belvalkar",
+  2023 - 1997,
+  "Student",
+  ["Jayesh", "Tushar", "Suraj"],
+];
+
+//! Objects Solves This Issue
+//! It Follows key:value Pairs, it is Known As Object Literal Syntax
+const ameyaObject = {
+  firstName: "Ameya",
+  lastName: "Belvalkar",
+  age: 2023 - 1997,
+  job: "Student",
+  friends: ["Jayesh", "Tushar", "Suraj"],
+};
+
+//! How To Read From Objects => Order Of Values Dosent Matter
+
+//? DOT operator
+console.log("Using . Operator", ameyaObject.firstName);
+console.log("Using . Operator", ameyaObject.age);
+
+//? [] "square Bracket Notation"
+
+console.log("Using [] Notaion", ameyaObject["firstName"]);
+console.log("Using [] Notaion", ameyaObject["age"]);
+
+//! Adding key:values to Object Using [] Operator
+ameyaObject["isMarried"] = false;
+
+console.log(ameyaObject);
