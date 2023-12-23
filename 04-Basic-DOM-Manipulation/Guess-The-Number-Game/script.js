c04c5da37c45ac7ed0259a45988c71aeccf6b47c
ameya-6964 <belvalkar7@gmail.com>
2023-12-24 02:01:15 +0530

    Played With Some Basic JavaScript

--- 04-Basic-DOM-Manipulation/Guess-The-Number-Game/script.js
+++ 04-Basic-DOM-Manipulation/Guess-The-Number-Game/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.querySelector('.message').textContent = '🥳 Correct Number 🥳 ';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+// NOTE => Input Field Contains Value Property
+document.querySelector('.guess').value = 23;
