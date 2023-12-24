5db04ec4cfc43406792afe04268c238597bfeed4
ameya-6964 <belvalkar7@gmail.com>
2023-12-24 22:27:42 +0530

    Completed Modal Code

--- 04-Basic-DOM-Manipulation/Modal-Window/script.js
+++ 04-Basic-DOM-Manipulation/Modal-Window/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const removeOverlay = () => {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", removeOverlay);
+}
+
+btnCloseModal.addEventListener("click", removeOverlay);
+
+overlay.addEventListener("click", removeOverlay);
