5dc30924c64f18c2add2b36d396feba0d66283ff
ameya-6964 <belvalkar7@gmail.com>
2023-12-24 22:37:51 +0530

    Handled esc key press event

--- 04-Basic-DOM-Manipulation/Modal-Window/script.js
+++ 04-Basic-DOM-Manipulation/Modal-Window/script.js
@@ -4,15 +4,23 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 
-const removeOverlay = () => {
+const closeModal = () => {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
+const openModal = () => {
   modal.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
 };
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener("click", removeOverlay);
+  btnOpenModal[i].addEventListener("click", openModal);
 }
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-btnCloseModal.addEventListener("click", removeOverlay);
-
-overlay.addEventListener("click", removeOverlay);
+document.addEventListener("keydown", function (e) {
+  if (e.key === `Escape`) {
+    closeModal();
+  }
+});
