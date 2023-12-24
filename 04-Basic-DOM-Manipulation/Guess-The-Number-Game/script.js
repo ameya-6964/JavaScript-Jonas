e2aad53feabdf9747ac3286d52a96dd4c9fe7998
ameya-6964 <belvalkar7@gmail.com>
2023-12-24 17:19:56 +0530

    Added Some Logic If Score Is Equal To 0

--- 04-Basic-DOM-Manipulation/Guess-The-Number-Game/script.js
+++ 04-Basic-DOM-Manipulation/Guess-The-Number-Game/script.js
@@ -9,12 +9,40 @@ document.querySelector('.guess').value = 23;
 */
 
 const button = document.querySelector('.check');
+const finalMessage = document.querySelector('.message');
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const diplayedNumber = document.querySelector('.number');
+diplayedNumber.textContent = secretNumber;
+let displayedScore = document.querySelector('.score');
+let score = 20;
 
 button.addEventListener('click', function (e) {
   e.preventDefault();
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number';
+    finalMessage.textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    finalMessage.textContent = '🥳 Correct Number';
+    score += 5;
+    displayedScore.textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      finalMessage.textContent = 'Too High 📈';
+      score--;
+      displayedScore.textContent = score;
+    } else {
+      finalMessage.textContent = 'You Lost The Game';
+      displayedScore.textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      finalMessage.textContent = 'Too Low 📉';
+      score--;
+      displayedScore.textContent = score;
+    } else {
+      finalMessage.textContent = 'You Lost The Game';
+      displayedScore.textContent = 0;
+    }
   }
 });
