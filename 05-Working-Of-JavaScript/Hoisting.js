78f7fc5c5cd4160b0f7f4cb9b8be57e8a8b5682b
ameya-6964 <belvalkar7@gmail.com>
2023-12-25 14:19:01 +0530

    added hoisting Noted

---
+++ 05-Working-Of-JavaScript/Hoisting.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//* Hoisting => Variables Lifted To Top Of There Scope
+//* Before Execution Code Is Scanned For Any Variable Declaration And For Each Declaration new Propert Is Created
+
+//! Function Declaration
+//* They Are Hoisted With Initial Value Is Actual Function
+
+//! var variables
+//* They Are Hoisted With initial Value Of undefine
+
+//! let and const variables
+//* They are Hoisted But they are in temporal Dead zone So They Cannot Be Accesed
+
+//! Function Expression and Arrow Scope
+//* Same As let and const (But It depends They Are Created with let or const)
+//* if Created with var they are hoisted With undefined Value
